9c668d41c43af828ea01294a8efa8ab720a39da8
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-10-30 09:48:24 +0700

    add detail movie

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { AuthContextProvider } from './context/AuthContext';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Account from './pages/Account';
-import ProtectedRoute from './components/ProtectedRoute';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
+import DetailMovie from "./pages/DetailMovie";
 
 function App() {
   return (
@@ -13,11 +14,12 @@ function App() {
       <AuthContextProvider>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<DetailMovie />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route
-            path='/account'
+            path="/account"
             element={
               <ProtectedRoute>
                 <Account />
